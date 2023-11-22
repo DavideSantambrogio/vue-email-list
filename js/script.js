f41c9a6fd6b9fe786164032f09906564fd5407da
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2023-11-22 10:21:56 +0100

    add: loading

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const app = createApp({
         // dati
         return{
             randomEmail: [],
+            isLoading: false,
         }
     },
 
@@ -16,14 +17,18 @@ const app = createApp({
     methods: {
         // funzioni
         generateEmail() {
+            this.randomEmail= []
+            this.isLoading =true;
             for (let i = 0; i < 10; i++) {
                 axios
                 .get("https://flynn.boolean.careers/exercises/api/random/mail")
                 .then((resp) => {
-                    this.randomEmail.push(resp.data.response);                  
+                    this.randomEmail.push(resp.data.response);  
+                    if (this.randomEmail.length === 10) {
+                        this.isLoading=false;                        
+                    }                
                 })                    
             }
         },
     },
 }).mount("#app");
-le
